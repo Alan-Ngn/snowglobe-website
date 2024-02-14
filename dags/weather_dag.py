ef15b592844156b9bddd77015b08de2f54571e9b
Alan Nguyen <alanngn21@gmail.com>
2024-02-13 21:42:13 -0800

    working on dag

---
+++ dags/weather_dag.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.http_operator import SimpleHttpOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner':'snowglobe',
+    'start_date': datetime(2023, 2, 14, 7, 0, 0),  # datetime(year, month, day, hour, minute, second)
+    'retries': 1,
+    'retries_delay': timedelta(minutes=15)
+}
+
+dag = DAG(
+    
+)
