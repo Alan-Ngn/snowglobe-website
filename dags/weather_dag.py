3c00039a3dd08adff11e61d150696600b4efc7d6
Alan Nguyen <alanngn21@gmail.com>
2024-02-23 10:45:55 -0800

    deleted commented code

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,6 +1,5 @@
 from airflow.decorators import dag, task
 from airflow.models import Variable
-# from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.providers.http.operators.http import HttpOperator
 from datetime import datetime, timedelta
 import json
@@ -41,46 +40,10 @@ def weather_etl():
             dict_result['weather']=i['weather'][0]['main']
             result.append(dict_result)
         return result
-        # x=data['list']
-        # result = {}
-        # result['name'] = "Crystal Mountain"
-        # result['date'] = datetime.utcfromtimestamp(x[0]['dt']).strftime('%Y-%m-%d %H:%M:%S UTC')
-        # print(result)
-        # return data['list']
+
 
 
     extracted_data = extract(api_results=get_weather_results_task.output)
     transformed_data = transform(extracted_data)
 
 weather_etl()
-# from airflow import DAG
-# from airflow.models import Variable
-# from airflow.operators.http_operator import SimpleHttpOperator
-# from datetime import datetime, timedelta
-
-# OPENWEATHERMAP_API_KEY = Variable.get("OPENWEATHERMAP_API_KEY")
-
-# default_args = {
-#     'owner':'snowglobe',
-#     'start_date': datetime(2023, 2, 14, 7, 0, 0),  # datetime(year, month, day, hour, minute, second)
-#     'retries': 1,
-#     'retries_delay': timedelta(minutes=15)
-# }
-
-# dag = DAG(
-#     'openweathermap',
-#     default_args=default_args,
-#     schedule_interval=timedelta(days=1)
-# )
-
-# weather_fetch_task = SimpleHttpOperator(
-#     task_id ='weather_fetch',
-#     method = 'GET',
-#     http_conn_id='openweathermap_api',
-#     endpoint=f'/data/2.5/forecast?lat={46.9282}&lon={}&appid={OPENWEATHERMAP_API_KEY}',
-#     headers={"Content-Type": "application/json"},
-#     dag=dag
-# )
-
-
-# weather_fetch_task
