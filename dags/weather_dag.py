e858165cc99e2307473d0e573b2eeb9d5ac419d9
Alan Nguyen <alanngn21@gmail.com>
2024-02-23 16:13:32 -0800

    working on multiple extracts

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -15,35 +15,77 @@ default_args = {
     schedule_interval=timedelta(days=1)
 )
 def weather_etl():
+    ski_resorts=[
+                    {
+                        'lat': 46.92930718381659,
+                        'lon':-121.50084437354852,
+                        'mountain': 'Crystal Mountain'
+                    },
+                    {
+                        'lat': 46.92930718381659,
+                        'lon':-121.50084437354852,
+                        'mountain': 'Summit at Snoqualmie'
+                    },
+                ]
+
+
+    # lat = 46.92930718381659
+    # lon =-121.50084437354852
     OPENWEATHERMAP_API_KEY = Variable.get("OPENWEATHERMAP_API_KEY")
-    get_weather_results_task = HttpOperator(
-        task_id ='weather_fetch',
-        method = 'GET',
-        http_conn_id='openweathermap_api',
-        endpoint=f'/data/2.5/forecast?lat={46.92930718381659}&lon={-121.50084437354852}&appid={OPENWEATHERMAP_API_KEY}&units=metric',
-        headers={"Content-Type": "application/json"},
-        do_xcom_push=True,
-    )
+    # get_weather_results_task = HttpOperator(
+    #     task_id ='weather_fetch',
+    #     method = 'GET',
+    #     http_conn_id='openweathermap_api',
+    #     endpoint=f'/data/2.5/forecast',
+    #     headers={"Content-Type": "application/json"},
+    #     data={
+    #         'lat':lat,
+    #         'lon':lon,
+    #         'appid':OPENWEATHERMAP_API_KEY,
+    #         'units':'metric'
+    #     },
+    #     do_xcom_push=True,
+    # )
 
     @task
-    def extract(api_results):
-        return json.loads(api_results)['list']
+    def extract(api_results, mountain):
+        return [mountain]+json.loads(api_results)['list']
 
     @task
     def transform(data):
-        result=[]
-        for i in data:
-            dict_result={}
-            dict_result['name'] = "Crystal Mountain"
-            dict_result['date'] = datetime.utcfromtimestamp(i['dt']).strftime('%Y-%m-%d %H:%M:%S UTC')
-            dict_result['temp'] = i['main']['temp']
-            dict_result['weather']=i['weather'][0]['main']
-            result.append(dict_result)
-        return result
+        # result=[]
+        # for i in data:
+        #     dict_result={}
+        #     dict_result['name'] = "Crystal Mountain"
+        #     dict_result['date'] = datetime.utcfromtimestamp(i['dt']).strftime('%Y-%m-%d %H:%M:%S UTC')
+        #     dict_result['temp'] = i['main']['temp']
+        #     dict_result['weather']=i['weather'][0]['main']
+        #     result.append(dict_result)
+        return data
+
+    result_test=[]
+    for i in ski_resorts:
+        mountain_task = i['mountain'].replace(' ', '_').lower()
+        get_weather_results_task = HttpOperator(
+            task_id =f'weather_fetch_{mountain_task}',
+            method = 'GET',
+            http_conn_id='openweathermap_api',
+            endpoint=f'/data/2.5/forecast',
+            headers={"Content-Type": "application/json"},
+            data={
+                'lat':i['lat'],
+                'lon':i['lon'],
+                'appid':OPENWEATHERMAP_API_KEY,
+                'units':'metric'
+            },
+            do_xcom_push=True,
+        )
+        result_test.append(extract(api_results=get_weather_results_task.output, mountain=i['mountain']))
 
 
 
-    extracted_data = extract(api_results=get_weather_results_task.output)
-    transformed_data = transform(extracted_data)
+
+    # extracted_data = extract(api_results=get_weather_results_task.output, mountain="Crystal Mountain")
+    transformed_data = transform(result_test)
 
 weather_etl()
